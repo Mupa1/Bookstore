755bb1c0b434328652e5f6ed3717cbfa31b9ecd4
Mupa1 <mupasmail@gmail.com>
2020-12-16 23:03:17 +0100

    implement App component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import BookList from '../containers/BookList';
+import BookForm from '../containers/BookForm';
+
+function App() {
+  return (
+    <div>
+      <BookList />
+      <BookForm />
+    </div>
+  );
+}
+
+export default App;

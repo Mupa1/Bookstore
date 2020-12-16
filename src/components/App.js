846fb9e3cadc9b5028239d610526901e5bc2813a
Mupa1 <mupasmail@gmail.com>
2020-12-16 23:49:51 +0100

    implement Book and BooksList components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import BookList from '../containers/BookList';
+import BookList from '../containers/BooksList';
 import BookForm from '../containers/BookForm';
 
 function App() {
---
+++ src/containers/BooksList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Book from '../components/Book';
+
+const BookList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Books ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {books.map(book => (
+        <Book key={book.id} book={book} />
+      ))}
+    </tbody>
+  </table>
+);
+
+BookList.propTypes = {
+  books: PropTypes.isRequired,
+};
+
+const mapStateToProps = state => ({ books: state.books });
+
+export default connect(mapStateToProps)(BookList);

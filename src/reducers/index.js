d601bb997018b1b252fc411df433390ee40fe544
Mupa1 <mupasmail@gmail.com>
2020-12-21 09:45:13 +0100

    add booksFilter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import booksReducer from './books';
+import booksFilter from './filter';
 
 const rootReducer = combineReducers({
   books: booksReducer,
+  filter: booksFilter,
 });
 
 export default rootReducer;

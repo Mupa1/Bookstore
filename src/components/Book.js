b54c39e1e61b44b894ab9e54d1ae1076334230fb
Mupa1 <mupasmail@gmail.com>
2020-12-27 09:57:31 +0100

    fix td nesting on table displaying books

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,26 +6,26 @@ const Book = ({ book, handleRemoveBook }) => {
 
   return (
     <tr className="book-container bg-white grey-bd row">
-      <div className="col-md-5 book-info-1 d-flex flex-column p-0 m-0">
-        <td className="montserrat font-weight-bold opacity p-0">{category}</td>
-        <td className="book-title font-weight-bold p-0">{title}</td>
-        <td className="author p-0">Jane Doe</td>
-        <div className="actions">
-          <td className="grey-right-bd comments">Comments</td>
-          <td className="grey-right-bd">
-            <button className="remove" type="button" onClick={() => handleRemoveBook(book)}>Remove</button>
-          </td>
-          <td className="edit">Edit</td>
+      <td className="col-md-5 book-info-1 d-flex flex-column p-0 m-0">
+        <p className="montserrat font-weight-bold opacity p-0 m-0">{category}</p>
+        <p className="book-title font-weight-bold p-0">{title}</p>
+        <p className="author p-0 m-0">Jane Doe</p>
+        <div className="actions d-flex">
+          <p className="grey-right-bd comments m-0">Comments</p>
+          <button className="remove grey-right-bd" type="button" onClick={() => handleRemoveBook(book)}>
+            Remove
+          </button>
+          <p className="edit m-0">Edit</p>
         </div>
-      </div>
-      <div className="col-md-3 d-flex align-items-center p-0 m-0">
+      </td>
+      <td className="col-md-3 d-flex align-items-center p-0 m-0">
         <div className="circle" />
         <div className="percent-info montserrat grey-right-bd m-0">
           <div className="percentage">75%</div>
           <div className="opacity">Completed</div>
         </div>
-      </div>
-      <div className="col-md-4 book-info-3 m-0">
+      </td>
+      <td className="col-md-4 book-info-3 m-0">
         <p className="opacity chapter-title m-0">CURRENT CHAPTER</p>
         <p className="chapter-num m-0">
           Chapter
@@ -33,7 +33,7 @@ const Book = ({ book, handleRemoveBook }) => {
           {id}
         </p>
         <button className="update" type="button">UPDATE PROGRESS</button>
-      </div>
+      </td>
     </tr>
   );
 };
